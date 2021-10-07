671a9a316425d4acd7f414db297b06f1fef12edb
ed <edwarbarrera@gmail.com>
2021-10-07 11:32:19 +0200

    first commit

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,28 @@
+import "./menu.scss"
+
+export default function Menu({menuOpen, setMenuOpen}) {
+    return (
+        <div className={"menu "+(menuOpen && "active")} id="menu">
+            <ul>
+                <li>
+                    <a href="#intro">Home</a>
+                </li>
+                <li>
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#testimonials">Testiomonials</a>
+                </li>
+                <li>
+                    <a href="#works">Works</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+
+
+            </ul>
+        </div>
+
+    )
+}
