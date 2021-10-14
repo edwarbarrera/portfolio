53e61a08baef320af38116b26bb365ac700b460b
ed <edwarbarrera@gmail.com>
2021-10-14 11:41:48 +0200

    wip portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,43 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App </li>
+                <li>Mobile App </li>
+                <li>Desing</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                     <img src="assets/spring boot app.png" alt="librairie" />
+                     <h3>librairie</h3>
+                </div>
+                <div className="item">
+                     <img src="assets/spring boot app.png" alt="librairie" />
+                     <h3>librairie</h3>
+                </div>
+                <div className="item">
+                     <img src="assets/spring boot app.png" alt="librairie" />
+                     <h3>librairie</h3>
+                </div>
+                <div className="item">
+                     <img src="assets/spring boot app.png" alt="librairie" />
+                     <h3>librairie</h3>
+                </div>
+                <div className="item">
+                     <img src="assets/spring boot app.png" alt="librairie" />
+                     <h3>librairie</h3>
+                </div>
+                <div className="item">
+                     <img src="assets/spring boot app.png" alt="librairie" />
+                     <h3>librairie</h3>
+                </div>
+            </div>
+                
         </div>
+            
+          
     )
 }
