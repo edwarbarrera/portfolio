8c0091ce691ffd900160de9bb01630544ac5adde
ed <edwarbarrera@gmail.com>
2021-10-14 17:05:25 +0200

    portfolio useEffet

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,45 +1,90 @@
+import { useEffect,useState } from "react"
+import PortfolioList from "../portfolioList/PortfolioList"
 import "./portfolio.scss"
+import{featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    designPortfolio,
+    contentPortfolio
+
+} from "../../data";
+
 
 export default function Portfolio() {
+    const [selected,setSelected]= useState("")
+    const [data,setData]= useState([])
+    const list = [
+        {
+            id:"featured",
+            title:"Featured",
+        },
+        {
+            id:"web",
+            title:"Web App",
+        },
+        {
+            id:"mobile",
+            title:"Mobile App",
+        },
+        {
+            id:"design",
+            title:"Design",
+        },
+        {
+            id:"content",
+            title:"Content",
+        }
+
+       
+    ]
+    useEffect(()=>{
+
+        switch(selected){
+            case"featured":
+            setData(featuredPortfolio);
+            break;
+            case"web":
+            setData(webPortfolio);
+            break;
+            case"mobile":
+            setData(mobilePortfolio);
+            break;
+            case"design":
+            setData(designPortfolio);
+            break;
+            case"content":
+            setData(contentPortfolio);
+            break;
+            default:
+                setData(featuredPortfolio)
+        }
+    }, [selected])
+
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className="active">Featured</li>
-                <li>Web App </li>
-                <li>Mobile App </li>
-                <li>Desing</li>
-                <li>Branding</li>
+               {list.map((item)=>(
+               <PortfolioList
+                title={item.title} 
+                active={selected === item.id} 
+                setSelected={setSelected}
+                id={item.id}
+                />))}
             </ul>
             <div className="container">
+                {data.map((d)=>(
                 <div className="item">
-                     <img src="assets/spring boot app.png" alt="librairie" />
-                     <h3>librairie</h3>
+                     <img 
+                     src={d.img}
+                     alt={d.title}
+                      />
+                     <h3>{d.title}</h3>
                 </div>
-                <div className="item">
-                     <img src="assets/spring boot app.png" alt="librairie" />
-                     <h3>librairie</h3>
-                </div>
-                <div className="item">
-                     <img src="assets/spring boot app.png" alt="librairie" />
-                     <h3>librairie</h3>
-                </div>
-                <div className="item">
-                     <img src="assets/spring boot app.png" alt="librairie" />
-                     <h3>librairie</h3>
-                </div>
-                <div className="item">
-                     <img src="assets/spring boot app.png" alt="librairie" />
-                     <h3>librairie</h3>
-                </div>
-                <div className="item">
-                     <img src="assets/spring boot app.png" alt="librairie" />
-                     <h3>librairie</h3>
-                </div>
+                ))}
             </div>
-                
         </div>
             
-          
-    )
+    );
 }
---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const featuredPortfolio = [
+    {
+        id:"1",
+        img:"https://www.pngkey.com/png/full/343-3434524_get-featured-only-5-more-days.png",
+        title:"Featured",
+    },
+    {
+        id:"2",
+        img:"https://myproblog.com/wp-content/uploads/2019/04/Active-Rain-featured-post.png",
+        title:"Web App stared",
+    }
+    
+
+]
+export const webPortfolio = [
+    {
+        id:"1",
+        img:"https://www.portices.fr/wp-content/uploads/2020/03/interet-world-wide-web.jpg",
+        title:"web",
+    },
+    {
+        id:"2",
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/GNOME_Web_logo--2018.svg/128px-GNOME_Web_logo--2018.svg.png",
+        title:"Web App",
+    }
+    
+]
+
+export const mobilePortfolio = [
+    {
+        id:"1",
+        img:"https://previews.123rf.com/images/aliasching/aliasching1409/aliasching140900223/31883419-t%C3%A9l%C3%A9phone-portable-dessin-anim%C3%A9.jpg",
+        title:"mobile",
+    },
+    {
+        id:"2",
+        img:"https://objectifsmartphone.fr/wp-content/uploads/2021/04/1617847037_826_gsmarena_001.jpg",
+        title:"Mobile App",
+    }
+    
+
+]
+export const designPortfolio = [
+    {
+        id:"1",
+        img:"https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/07/bd2c1adb-eb6d-496f-8fe0-1916ce3f7306.png?auto=format&q=60&fit=max&w=930",
+        title:"desing",
+    },
+    {
+        id:"2",
+        img:"https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/Ultimate-guide-to-app-design-2-700x410.jpg?auto=format&q=60&fit=max&w=930",
+        title:"desing mobile App",
+    }
+    
+
+]
+export const contentPortfolio = [
+    {
+        id:"1",
+        img:"https://www.blogsaays.com/wp-content/uploads/2019/09/Diffrent-types-of-content-for-bloggers.jpg",
+        title:"content",
+    },
+    {
+        id:"2",
+        img:"http://www.v-com.fr/wp-content/uploads/2016/10/content-marketing-services-santa-cruz-02-1170x550.png",
+        title:"content mobile App",
+    }
+    
+
+]
