6b7a01f7c08e3e55f7eff05dbee61cf11e4c9723
ed <edwarbarrera@gmail.com>
2021-10-15 10:20:19 +0200

    slider ok

--- src/components/travail/Travail.jsx
+++ src/components/travail/Travail.jsx
@@ -1,38 +1,74 @@
+import { useState } from "react";
 import "./travail.scss"
 
 export default function Travail() {
+
+        const[currentSlide, setCurrentSlide]=useState(0)
+    const data = [
+
+        {
+            id: "1",
+            icon: "./assets/mobile.png",
+            title: "Mobile Application",
+            desc: "Wip",
+            img: "https://vupartout.com/public/uploads/services/Mobile-App-Development.png",
+
+        },
+        {
+            id: "2",
+            icon: "./assets/writing.png",
+            title: "branding",
+            desc: "Wip",
+            img: "https://www.whiteriverdesign.com/wp-content/uploads/2016/11/Branding-v-marketing.png",
+
+        },
+        {
+            id: "3",
+            icon: "./assets/globe.png",
+            title: "SpringBoot react",
+            desc: "Wip",
+            img: "./assets/spring boot app.png",
+
+        }
+    ]
+const handleClick= (way)=>{
+    way==="left" ? setCurrentSlide(currentSlide>0 ? currentSlide-1 :2) :
+    setCurrentSlide(currentSlide<data.length-1? currentSlide +1:0)
+}
     return (
         <div className="travail" id="travail">
-            <div className="slider">
-                <div className="container">
-                    <div className="item">
-                        <div className="left">
-                        <div className="left">
-                            <div className="leftContainer">
-                                <div className="imgContainer">
-                                    <img src="assets/mobile.png" alt="mobile" />
+            <div className="slider" style={{transform:`translateX(-${currentSlide *100}vw)`}}>
+                {data.map(d => (
+                    <div className="container">
+
+                        <div className="item">
+                            <div className="left">
+                                <div className="left">
+                                    <div className="leftContainer">
+                                        <div className="imgContainer">
+                                            <img src={d.icon} alt={d.desc} />
+                                        </div>
+                                        <h2>{d.title}</h2>
+                                        <p>
+                                            <h2>Description</h2>
+                                            {d.desc}  
+                                        </p>
+                                        <span>Projects</span>
+
+
+                                    </div>
                                 </div>
-                                <h2>Titre</h2>
-                                <p>
-                                <h2>Description</h2>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                </p>
-                                <span>Projects</span>
+                                <div className="right">
+                                    <img src={d.img} alt="projects" />
 
-
+                                </div>
                             </div>
                         </div>
-                        <div className="right">
-                            <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/07/7cd7150cdc497d419515fb465e8088de-e1531145136824.jpg?auto=format&q=60&fit=max&w=930" alt="projects" />
+                    </div>))}
 
-                        </div>
-                    </div>
-                </div>
             </div>
-            
-         </div>
-         <img src="assets/right-arrow.png" className="arrow left" alt="left arrow" />
-            <img src="assets/right-arrow.png" className="arrow right" alt="right arrow" />
-            </div>
+            <img src="assets/right-arrow.png" className="arrow left" alt="left arrow"  onClick= {()=>handleClick("left")}/>
+            <img src="assets/right-arrow.png" className="arrow right" alt="right arrow" onClick= {()=>handleClick()} />
+        </div>
     );
 }
